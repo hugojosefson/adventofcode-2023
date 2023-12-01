c9268daab02ed61d814c97220e7fa3770a02e9e7
Hugo Josefson <hugo@josefson.org>
2023-12-01 16:55:51 +0100

    chore: fix dependency

--- src/01/01.ts
+++ src/01/01.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.208.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.208.0/assert/assert_equals.ts";
 
 export type CalibrationNumbers = [number, number];
 export function extractCalibrationNumbers(input: string): number[] {
